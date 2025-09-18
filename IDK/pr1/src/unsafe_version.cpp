2b6e06fa07c95d3e0ebfe81fffecf64b89726276
star.barsuk <120922249+Hodilton@users.noreply.github.com>
2025-09-18 17:09:53 +0200

    unsafe-mutex-atomic counters

---
+++ IDK/pr1/src/unsafe_version.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <thread>
+
+int main()
+{
+    unsigned long long g_count = 0;
+
+    std::thread t1([&]()
+                   {
+        for(auto i = 0; i < 1'000'000; ++i)
+            ++g_count; });
+
+    std::thread t2([&]()
+                   {
+        for(auto i = 0; i < 1'000'000; ++i)
+            ++g_count; });
+
+    t1.join();
+    t2.join();
+
+    std::cout << g_count << std::endl;
+
+    return 0;
+}
