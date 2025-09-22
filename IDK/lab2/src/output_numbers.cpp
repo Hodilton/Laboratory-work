217bff19ddbddc1c9433cd6da3b88b1e5e96ad20
star.barsuk <120922249+Hodilton@users.noreply.github.com>
2025-09-22 09:48:42 +0200

    lab2 for idk

---
+++ IDK/lab2/src/output_numbers.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <mutex>
+
+int counter1 = 1;        // Counter for first stream
+int counter2 = 1;        // Counter for second stream
+bool turn = true;        // Thread queue (true - first thread, false - second)
+int numbersToPrint1 = 1; // The number of numbers to output by first stream
+int numbersToPrint2 = 1; // The number of numbers to output by second stream
+std::mutex mtx;          // Mutex for sync
+
+void threadFirstFunction()
+{
+    while (counter1 <= 100)
+    {
+        std::lock_guard<std::mutex> lock(mtx);
+
+        if (turn)
+        {
+            for (int i = 0; i < numbersToPrint1 && counter1 <= 100; i++)
+            {
+                std::cout << counter1 << " ";
+                counter1++;
+            }
+
+            turn = false;
+
+            numbersToPrint1++;
+
+            if (numbersToPrint1 > 10)
+                numbersToPrint1 = 1;
+        }
+    }
+}
+
+void threadSecondFunction()
+{
+    while (counter2 <= 100)
+    {
+        std::lock_guard<std::mutex> lock(mtx);
+
+        if (!turn)
+        {
+            for (int i = 0; i < numbersToPrint2 && counter2 <= 100; i++)
+            {
+                std::cout << counter2 << " ";
+                counter2++;
+            }
+
+            turn = true;
+
+            numbersToPrint2++;
+
+            if (numbersToPrint2 > 10)
+                numbersToPrint2 = 1;
+        }
+    }
+}
+
+int main()
+{
+    std::thread thread1(threadFirstFunction);
+    std::thread thread2(threadSecondFunction);
+
+    thread1.join();
+    thread2.join();
+
+    std::cout << std::endl;
+
+    return 0;
+}
---
+++ IDK/lab2/src/sort_array.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <thread>
+
+void quickSort(int arr[], int start, int end)
+{
+    if (start >= end)
+        return;
+
+    int anchor = arr[end];
+    int i = start - 1;
+
+    for (int j = start; j < end; j++)
+    {
+        if (arr[j] <= anchor)
+        {
+            i++;
+
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+
+    int temp = arr[i + 1];
+    arr[i + 1] = arr[end];
+    arr[end] = temp;
+
+    int anchorIndex = i + 1;
+
+    quickSort(arr, start, anchorIndex - 1);
+    quickSort(arr, anchorIndex + 1, end);
+}
+
+void printArray(int arr[], int size, const std::string &message)
+{
+    std::cout << message;
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+int main()
+{
+    const int size = 10;
+    int array[size] = {5, 34, 57, 86, 3, 87, 43, 25, 11, 14};
+
+    printArray(array, size, "Source array: ");
+
+    const int mid = size / 2;
+
+    std::thread thread1(quickSort, array, 0, mid - 1);
+    std::thread thread2(quickSort, array, mid, size - 1);
+
+    thread1.join();
+    thread2.join();
+
+    printArray(array, size, "Result array: ");
+
+    return 0;
+}
