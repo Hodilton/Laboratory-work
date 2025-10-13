06d14ccc2e2d989ed7fbc11d4bdc6d46487670ff
star.barsuk <120922249+Hodilton@users.noreply.github.com>
2025-10-13 12:28:59 +0200

    add signature methods

---
+++ RAD/lab6/src/graph/graph.h
@@ -0,0 +1,29 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <vector>
+#include <iostream>
+
+class Graph {
+public:
+    Graph(const std::vector<std::vector<int>>& matrix);
+
+private:
+    std::vector<std::vector<int>> adjacencyMatrix;
+    int n;
+
+private:
+    bool isValidMatrix() const;
+
+    bool isSimple() const;
+
+    bool isNullGraph() const;
+
+    int maxDegree() const;
+
+    bool hasLeafVertices() const;
+
+    void printMatrix() const;
+};
+
+#endif // GRAPH_H
